c1aaf895bcfce1fa8f7323271f05895d3c537330
Wallyson Jhonatan de Oliveira <waloli@futura.sbo>
2023-10-18 18:26:47 -0300

    add order dashboard component

---
+++ src/components/BaseOrders.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- Base Message  -->
+<!-- 
+    Order histories
+
+    Filter  by Name / ID / Status 
+    ID - Name - Bread - Steak - optionals - Burguer status
+    Actions on order: Cancel (canceled + date) or confirm Burguer (completed + date) -->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BaseOrders',
+  }
+</script>
+
+<style scoped>
+
+</style>
